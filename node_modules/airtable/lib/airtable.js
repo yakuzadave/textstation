0ce0340a9d252607f372308dd820ccc59df72bf6
Dave Carmocan <yakuzadave@gmail.com>
2019-01-10 13:15:49 -0800

    Firdt commit

---
+++ node_modules/airtable/lib/airtable.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var assert = require('assert');
+
+var Class = require('./class');
+var Base = require('./base');
+var Record = require('./record');
+var Table = require('./table');
+var AirtableError = require('./airtable_error');
+
+var Airtable = Class.extend({
+    init: function(opts) {
+        opts = opts || {};
+
+        var default_config = Airtable.default_config();
+
+        this._apiKey = opts.apiKey || Airtable.apiKey || default_config.apiKey;
+        this._endpointUrl = opts.endpointUrl || Airtable.endpointUrl || default_config.endpointUrl;
+        this._apiVersion = opts.apiVersion || Airtable.apiVersion || default_config.apiVersion;
+        this._apiVersionMajor = this._apiVersion.split('.')[0];
+        this._allowUnauthorizedSsl = opts.allowUnauthorizedSsl || Airtable.allowUnauthorizedSsl || default_config.allowUnauthorizedSsl;
+        this._noRetryIfRateLimited = opts.noRetryIfRateLimited || Airtable.noRetryIfRateLimited || default_config.noRetryIfRateLimited;
+        this.requestTimeout = opts.requestTimeout || default_config.requestTimeout;
+
+        assert(this._apiKey, 'API key is required to connect to Airtable');
+    },
+
+    base: function(baseId) {
+        return Base.createFunctor(this, baseId);
+    }
+});
+
+Airtable.default_config = function () {
+    return {
+        endpointUrl: process.env.AIRTABLE_ENDPOINT_URL || 'https://api.airtable.com',
+        apiVersion: '0.1.0',
+        apiKey: process.env.AIRTABLE_API_KEY,
+        allowUnauthorizedSsl: false,
+        noRetryIfRateLimited: false,
+        requestTimeout: 300 * 1000, // 5 minutes
+    };
+};
+
+Airtable.configure = function(opts) {
+    Airtable.apiKey = opts.apiKey;
+    Airtable.endpointUrl = opts.endpointUrl;
+    Airtable.apiVersion = opts.apiVersion;
+    Airtable.allowUnauthorizedSsl = opts.allowUnauthorizedSsl;
+    Airtable.noRetryIfRateLimited = opts.noRetryIfRateLimited;
+};
+
+Airtable.base = function(baseId) {
+    return new Airtable().base(baseId);
+};
+
+Airtable.Base = Base;
+Airtable.Record = Record;
+Airtable.Table = Table;
+Airtable.Error = AirtableError;
+
+module.exports = Airtable;
