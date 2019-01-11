94d7c654b57d6724a75906f5348ade86417827fb
Dave Carmocan <yakuzadave@gmail.com>
2019-01-10 16:30:28 -0800

    Adding Express and Pug

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const fetch = require('node-fetch')
 const request = require('request');
+const pug = require('pug')
 
 
-request('http://www.google.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
 
-
-fetch('https://ptb.discordapp.com/api/webhooks/528021933275480085/ECzbLFsIWMSzMxvBe8Siz7AHRlLbL5tibfyqn5vKy2FBozUp4te97aSbl9mLAn4k0R_J')
-.then(res => res.json())
-.then(json => console.log(body));
